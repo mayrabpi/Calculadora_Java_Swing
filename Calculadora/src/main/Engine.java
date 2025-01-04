c3018f52ca38a7185703685347bd1e1d1bc4058b
mayrabpi <145108717+mayrabpi@users.noreply.github.com>
2025-01-04 23:39:18 +0100

    botones

--- Calculadora/src/main/Engine.java
+++ Calculadora/src/main/Engine.java
@@ -3,6 +3,7 @@ package main;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -18,6 +19,23 @@ public class Engine implements ActionListener {
 	private JPanel buttonPanel;//panel sur que contiene los botones
 	private JTextField display;//display
 	//Botones
+	private JButton n0;
+	private JButton n1;
+	private JButton n2;
+	private JButton n3;
+	private JButton n4;
+	private JButton n5;
+	private JButton n6;
+	private JButton n7;
+	private JButton n8;
+	private JButton n9;
+	private JButton divide;
+	private JButton multiplica;
+	private JButton resta;
+	private JButton suma;
+	private JButton igual;
+	private JButton borra;
+	private JButton negativo;
 
 	
 	
